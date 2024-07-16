b3f41db2cc19e02e7da761ba235088cdc57f2cd6
YashDEVP <dinesh1328gupta@gmail.com>
2024-07-15 21:40:12 -0700

    Code setup for Uber App

---
+++ src/test/java/com/codingshuttle/project/uber/uberApp/UberAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.project.uber.uberApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
