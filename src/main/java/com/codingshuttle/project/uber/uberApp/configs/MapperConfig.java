27da3ecbe34906a4b091f47c5607222378d59cd9
YashDEVP <dinesh1328gupta@gmail.com>
2024-07-23 20:46:13 -0700

    name change of class

--- src/main/java/com/codingshuttle/project/uber/uberApp/configs/MapperConfig.java
+++ src/main/java/com/codingshuttle/project/uber/uberApp/configs/MapperConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 public class MapperConfig {
 
     @Bean
-    ModelMapper modelMpper(){
+    ModelMapper modelMapper(){
         return new ModelMapper();
     }
 }
